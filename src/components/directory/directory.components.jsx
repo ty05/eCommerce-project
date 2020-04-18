3c82e7bfca16c5ab5ac2e1c561c5872897024ec2
ty05 <yanataka5@hotmail.com>
2020-04-18 22:18:23 +0800

    broken function

---
+++ src/components/directory/directory.components.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Sections from './sections';
+import MenuItem from '../menu-item/menu-item.components';
+import './directory.styles.scss';
+
+
+function Directory(){
+
+    return(
+        <div className='directory-menu'>
+            {Sections.map((section,index) => (
+                <MenuItem 
+                key={index}
+                id={section.id}
+                title={section.title}
+                imagUrl={section.imageUrl}
+                size={section.size}
+                ></MenuItem>
+            ))}
+        </div>
+    )
+}
+
+export default Directory;
